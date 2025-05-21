3c8f0b7b8b731426ffb6208f351d5c4e95516db2
HelloAle <ale.oliveira.deet@gmail.com>
2025-05-21 09:18:10 -0300

    readme i guess

---
+++ Sleep Better/node_modules/react-native-fbsdk-next/lib/typescript/RNFBSDKExample/App.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+/// <reference types="react" />
+declare function App(): JSX.Element;
+export default App;
